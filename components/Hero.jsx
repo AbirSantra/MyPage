ff4b358deaebcaf88335464d86cbaa3f8e7e1073
Abir Santra <santraabir8@gmail.com>
2022-07-11 20:38:31 +0530

    Designed Hero Section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,33 +2,38 @@ import React from "react";
 import Image from "next/image";
 import Avatar from "../public/abir dp.png";
 import Socials from "./Socials";
+import { BiChevronsDown } from "react-icons/bi";
 
 const Hero = () => {
   return (
-    <div className="h-[calc(100vh-10rem)] md:h-[calc(100vh-6rem)] w-full flex justify-center items-center">
+    <div className="relative h-[calc(100vh-10rem)] md:h-[calc(100vh-6rem)] w-full flex justify-center items-center">
       <div className="w-full max-w-[1440px] h-full p-4 md:p-8 lg:p-16 flex flex-col justify-center items-center gap-4 text-center text-dark-black dark:text-white-two">
         <p className="text-main-dark font-semibold text-xl uppercase">
-          Hi, I'm Abir
+          Hi, I&apos;m Abir
         </p>
         <h1 className="font-Titillium text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight">
           Full Stack Developer & Graphics Designer
         </h1>
-        <p className="text-md md:text-2xl mt-4">
-          Just a guy who loves building things out of code.
+        <p className="text-md md:text-2xl mt-4 text-light-grey">
+          I love building things out of code.
         </p>
         <div
-          className="bg-main-dark rounded-full w-[200px] h-[200px] flex justify-center items-center animate-floating-div transition-all ease-in duration-200 mt-8"
+          className="bg-main-dark rounded-full w-[170px] h-[170px] flex justify-center items-center animate-floating-div transition-all ease-in duration-200 mt-8"
           title="Yeah, that's my face😐"
         >
           <Image
             src={Avatar}
             alt="Abir Avatar"
-            width={190}
-            height={190}
-            className="relative z-10"
+            width={160}
+            height={160}
+            // className="relative z-10"
           />
         </div>
         <Socials />
+        <BiChevronsDown
+          size={24}
+          className="absolute bottom-2 sm:bottom-8 cursor-pointer text-light-grey animate-floating-div"
+        />
       </div>
     </div>
   );
