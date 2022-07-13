be6e1c53bd8f9fe18bdc9d1a1c4fa422b1a92412
Abir Santra <santraabir8@gmail.com>
2022-07-13 22:09:23 +0530

    Completed About section and added resume file

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ServiceCard from "./ServiceCard";
 import { FaCode } from "react-icons/fa";
-import { FiPenTool } from "react-icons/fi";
+import { FiPenTool, FiDownload } from "react-icons/fi";
 import { MdOutlineDesignServices } from "react-icons/md";
+import { IoOpenOutline } from "react-icons/io5";
 
 const About = () => {
   // Outer Div
@@ -32,7 +33,45 @@ const About = () => {
           />
         </div>
         {/* Right Section - About */}
-        <div className="w-full flex-1">About me</div>
+        <div className="w-full flex-1 flex flex-col justify-center items-start gap-6">
+          <h1 className="font-Titillium text-dark-black dark:text-white-two">
+            About Me
+          </h1>
+          <h2 className="font-Titillium font-medium text-xl italic text-light-black dark:text-white-two">
+            I enjoy creating things that make a difference.
+          </h2>
+          <p className="text-light-grey text-sm font-medium sm:text-base">
+            I started coding in high school and currently am a second-year
+            undergraduate studying Information Technology at MCKV Institute of
+            Engineering, looking for opportunities. <br /> <br />
+            I'm pretty positive and strong-willed and I always finish what I
+            start. My dream is to be happy in life, doing what I love and being
+            surrounded by my loved ones. When I'm not coding, I love to hang out
+            with my friends and play video games, listen to music, jam on my
+            drums, and watch Anime.
+          </p>
+          <div className="flex items-center justify-center gap-6">
+            <p className=" text-dark-black dark:text-white-two text-lg font-medium font-Titillium">
+              Resume:{" "}
+            </p>
+            <a href="/Abir Santra CV(Unfinished).pdf" download>
+              <FiDownload
+                size={24}
+                className="text-dark-black dark:text-white-two hover:text-main-dark duration-300 ease-in-out dark:hover:text-main-dark"
+              />
+            </a>
+            <a
+              href="/Abir Santra CV(Unfinished).pdf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IoOpenOutline
+                size={24}
+                className="text-dark-black dark:text-white-two hover:text-main-dark duration-300 ease-in-out dark:hover:text-main-dark"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
