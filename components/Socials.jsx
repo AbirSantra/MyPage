74251c55c0e47a597523e4d59e54a25daa0cf361
Abir Santra <santraabir8@gmail.com>
2022-07-13 20:40:30 +0530

    Changed Social Icons background for light mode

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -7,25 +7,25 @@ const Socials = () => {
     <div className="flex justify-center items-center gap-4 md:gap-8 mt-12">
       <a
         href=""
-        className="text-2xl md:text-2xl text-light-grey hover:text-white-two dark:hover:text-dark-black ease-in-out duration-300   rounded-full p-2 bg-white-one dark:bg-dark-black hover:bg-main-dark dark:hover:bg-main-dark"
+        className="text-2xl md:text-2xl text-light-grey hover:text-white-two dark:hover:text-dark-black ease-in-out duration-300   rounded-full p-2 bg-white-two dark:bg-dark-black hover:bg-main-dark dark:hover:bg-main-dark"
       >
         <FaGithub></FaGithub>
       </a>
       <a
         href=""
-        className="text-2xl md:text-2xl text-light-grey hover:text-white-two dark:hover:text-dark-black ease-in-out duration-300   rounded-full p-2 bg-white-one dark:bg-dark-black hover:bg-main-dark dark:hover:bg-main-dark"
+        className="text-2xl md:text-2xl text-light-grey hover:text-white-two dark:hover:text-dark-black ease-in-out duration-300   rounded-full p-2 bg-white-two dark:bg-dark-black hover:bg-main-dark dark:hover:bg-main-dark"
       >
         <FaLinkedinIn></FaLinkedinIn>
       </a>
       <a
         href=""
-        className="text-2xl md:text-2xl text-light-grey hover:text-white-two dark:hover:text-dark-black ease-in-out duration-300   rounded-full p-2 bg-white-one dark:bg-dark-black hover:bg-main-dark dark:hover:bg-main-dark"
+        className="text-2xl md:text-2xl text-light-grey hover:text-white-two dark:hover:text-dark-black ease-in-out duration-300   rounded-full p-2 bg-white-two dark:bg-dark-black hover:bg-main-dark dark:hover:bg-main-dark"
       >
         <FaTwitter></FaTwitter>
       </a>
       <a
         href=""
-        className="text-2xl md:text-2xl text-light-grey hover:text-white-two dark:hover:text-dark-black ease-in-out duration-300   rounded-full p-2 bg-white-one dark:bg-dark-black hover:bg-main-dark dark:hover:bg-main-dark"
+        className="text-2xl md:text-2xl text-light-grey hover:text-white-two dark:hover:text-dark-black ease-in-out duration-300   rounded-full p-2 bg-white-two dark:bg-dark-black hover:bg-main-dark dark:hover:bg-main-dark"
       >
         <HiMailOpen></HiMailOpen>
       </a>
