3fc72869fa628c75d7da2068102743c9275f2be7
Abir Santra <santraabir8@gmail.com>
2022-07-13 21:05:04 +0530

    Added Scale and title color to services card on hover

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const ServiceCard = ({ name, icon, desc }) => {
   // Container Div
   return (
-    <div className="w-full h-fit flex justify-center items-start bg-grey dark:bg-light-black rounded-lg p-6 gap-8 border-[1px] border-grey dark:border-light-black hover:border-[1px] hover:border-main-dark dark:hover:border-main-dark ease-in-out duration-300">
+    <div className="group w-full h-fit flex justify-center items-start bg-grey dark:bg-light-black rounded-lg p-6 gap-8 border-[1px] border-grey dark:border-light-black hover:border-[1px] hover:border-main-dark dark:hover:border-main-dark ease-in-out duration-300 cursor-pointer hover:scale-105">
       {/* Text */}
       <div className="w-full flex justify-center items-start flex-col gap-2">
-        <h1 className="text-[1rem] sm:text-[1.5rem] font-Titillium text-dark-black dark:text-white-two">
+        <h1 className="text-[1rem] sm:text-[1.2rem] font-semibold tracking-wide font-Titillium text-dark-black dark:text-white-two group-hover:text-main-dark ease-in-out duration-300">
           {name}
         </h1>
         <p className="text-[0.8rem] sm:text-[0.9rem] text-dark-black dark:text-light-grey">
