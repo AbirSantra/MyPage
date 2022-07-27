c01a63ea31f79ecf2dbee77d23253efbe86718fa
Abir Santra <santraabir8@gmail.com>
2022-07-28 00:07:30 +0530

    Changed Project Section y-padding

--- components/ProjectSection.jsx
+++ components/ProjectSection.jsx
@@ -9,7 +9,7 @@ const ProjectSection = () => {
   return (
     <div className="w-full min-h-screen flex justify-center items-center py-28">
       {/* Container Div */}
-      <div className="w-full max-w-[1440px] flex flex-col gap-16 justify-center items-start p-4 md:p-8 lg:p-16">
+      <div className="w-full max-w-[1440px] flex flex-col gap-16 justify-center items-start px-4 md:px-8 lg:px-16">
         {/* Title Div */}
         <div className="flex justify-center items-start flex-col gap-4">
           <h1 className="font-Titillium  text-dark-black dark:text-white-two">
