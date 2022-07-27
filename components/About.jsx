e6255b38abde23d90dc60dee33cd41392a7fa63b
Abir Santra <santraabir8@gmail.com>
2022-07-28 00:06:03 +0530

    Changed Padding

--- components/About.jsx
+++ components/About.jsx
@@ -14,9 +14,15 @@ const About = () => {
       id="about"
     >
       {/* Container Div */}
-      <div className="w-full max-w-[1440px] flex flex-col-reverse md:flex-row justify-center items-center flex-wrap-reverse p-4 md:p-8 lg:p-16 gap-16 ">
+      <div className="w-full max-w-[1440px] flex flex-col-reverse md:flex-row justify-center items-center flex-wrap-reverse p-4 md:p-8 lg:p-16 gap-32 md:gap-16 ">
         {/* Left Section - Services */}
-        <div className="w-full flex-1 flex flex-col justify-center items-center gap-6">
+        <div className="w-full flex-1 flex flex-col justify-center items-start gap-4">
+          <h1 className="md:hidden font-Titillium text-dark-black dark:text-white-two">
+            Skills
+          </h1>
+          <p className="md:hidden text-light-grey text-sm font-medium sm:text-base">
+            These are the things that I'm good at
+          </p>
           <ServiceCard
             name="Web Development"
             desc="This is just a sample description. This paragraph will briefly describe the service in about 1-2 lines."
@@ -34,13 +40,10 @@ const About = () => {
           />
         </div>
         {/* Right Section - About */}
-        <div className="w-full flex-1 flex flex-col justify-center items-start gap-6">
+        <div className="w-full flex-1 flex flex-col justify-center items-start gap-8">
           <h1 className="font-Titillium text-dark-black dark:text-white-two">
             About Me
           </h1>
-          {/* <h2 className="font-Titillium font-medium text-2xl italic text-light-black dark:text-white-two">
-            I enjoy creating things that make a difference.
-          </h2> */}
           <p className="text-light-grey text-sm font-medium sm:text-base">
             I enjoy creating things that make a difference
           </p>
@@ -55,25 +58,22 @@ const About = () => {
             jam on my drums, and watch Anime.
           </p>
           <div className="flex items-center justify-center gap-4 text-sm sm:text-base">
-            {/* <p className=" text-dark-black dark:text-white-two text-lg font-medium font-Titillium">
-              Resume:{" "}
-            </p> */}
-            <a
-              href="/Abir Santra CV(Unfinished).pdf"
-              download
-              className="relative btn"
-            >
-              Resume
-              <FiDownload />
-            </a>
             <a
               href="/Abir Santra CV(Unfinished).pdf"
               target="_blank"
               rel="noreferrer"
-              className="btn-2"
+              className="btn"
             >
               Learn More
               <BsArrowRight />
+            </a>
+            <a
+              href="/Abir Santra CV(Unfinished).pdf"
+              download
+              className="btn-2"
+            >
+              Resume
+              <FiDownload />
             </a>
           </div>
         </div>
