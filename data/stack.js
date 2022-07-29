d2a2c69937eee25df2fb9099a3303cd9431d4837
Abir Santra <santraabir8@gmail.com>
2022-07-29 20:18:11 +0530

    Completed Stack Page

---
+++ data/stack.js
@@ -0,0 +1,124 @@
+import bootstrap from "../img/bootstrap-logo.png";
+import clang from "../img/c.png";
+import canva from "../img/canva.png";
+import cpp from "../img/cpp.png";
+import css3 from "../img/css-3 (1).png";
+import expressjs from "../img/expressjs.png";
+import figma from "../img/figma.png";
+import html5 from "../img/html-5.png";
+import illustrator from "../img/illustrator.png";
+import java from "../img/java.png";
+import javascript from "../img/js.png";
+import mongodb from "../img/mongodb.png";
+import nodejs from "../img/node-js.png";
+import photoshop from "../img/photoshop.png";
+import python from "../img/python.png";
+import reactjs from "../img/reactjs.png";
+import sass from "../img/sass.png";
+import sanity from "../img/sanity.png";
+import redux from "../img/redux1.png";
+import tailwind from "../img/tailwind.png";
+import next from "../img/next.svg";
+
+export const techStack = [
+  {
+    name: "HTML5",
+    icon: html5,
+    desc: "Web Design",
+  },
+  {
+    name: "CSS3",
+    icon: css3,
+    desc: "Web Design",
+  },
+  {
+    name: "Javascript",
+    icon: javascript,
+    desc: "Programming",
+  },
+  {
+    name: "React",
+    icon: reactjs,
+    desc: "Frontend",
+  },
+  {
+    name: "NextJS",
+    icon: next,
+    desc: "Web Development",
+  },
+  {
+    name: "NodeJS",
+    icon: nodejs,
+    desc: "Backend",
+  },
+  {
+    name: "ExpressJS",
+    icon: expressjs,
+    desc: "Backend",
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+    desc: "Database",
+  },
+  {
+    name: "Redux",
+    icon: redux,
+    desc: "State Management",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+    desc: "CSS Framework",
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+    desc: "CSS Library",
+  },
+  {
+    name: "Sanity",
+    icon: sanity,
+    desc: "Content Management",
+  },
+  {
+    name: "C",
+    icon: clang,
+    desc: "Programming",
+  },
+  {
+    name: "C++",
+    icon: cpp,
+    desc: "Programming",
+  },
+  {
+    name: "Java",
+    icon: java,
+    desc: "Programming",
+  },
+  {
+    name: "Python",
+    icon: python,
+    desc: "Programming",
+  },
+  {
+    name: "Photoshop",
+    icon: photoshop,
+    desc: "Graphic Design",
+  },
+  {
+    name: "Illustrator",
+    icon: illustrator,
+    desc: "Graphic Design",
+  },
+  {
+    name: "Canva",
+    icon: canva,
+    desc: "Graphic Design",
+  },
+  {
+    name: "Figma",
+    icon: figma,
+    desc: "UI Design",
+  },
+];
--- pages/tech.js
+++ pages/tech.js
@@ -1,7 +1,39 @@
+import Head from "next/head";
 import React from "react";
+import { techStack } from "../data/stack";
+import StackCard from "../components/StackCard";
 
 const tech = () => {
-  return <div>tech</div>;
+  return (
+    <div className="w-full flex justify-center items-center py-20">
+      <div className="w-full max-w-[1440px] flex flex-col justify-center items-center gap-16 px-4 md:px-8 lg:px-16">
+        <Head>
+          <title>Abir Santra | Tech Stack</title>
+          <meta name="description" content="Abir Santra's portfolio" />
+          <link rel="icon" href="/abir dp.png" />
+        </Head>
+
+        {/* Header Div */}
+        <div className="flex justify-center items-center flex-col gap-4">
+          <h1 className="font-Titillium  text-dark-black dark:text-white-two">
+            Tech Stack
+          </h1>
+          <p className="text-main-dark text-xl font-medium text-center">
+            These are my everyday go-to tools and technologies that I use
+          </p>
+        </div>
+
+        {/* Tech Div */}
+        <div className="w-full grid justify-center items-center gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          {techStack.map((card) => {
+            return (
+              <StackCard icon={card.icon} name={card.name} desc={card.desc} />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default tech;
