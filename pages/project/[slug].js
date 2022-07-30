04d68843e4b01a299d5f241dee959f6087fb94d4
Abir Santra <santraabir8@gmail.com>
2022-07-30 22:20:20 +0530

    Minor Design changes

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -50,13 +50,35 @@ const Project = ({ project }) => {
         <div className="w-full flex flex-col justify-center items-start gap-8">
           {/* Header */}
           <div className="w-full flex flex-col justify-center items-start gap-4">
+            <h1 className="text-dark-black dark:text-white-one">{title}</h1>
+            <p className=" text-dark-black dark:text-white-one">{subtitle}</p>
             <img
               src={urlFor(coverImage).url()}
               alt={title}
-              className="w-full rounded-2xl mb-4"
+              className="w-full rounded-2xl my-4"
             />
-            <h1 className="text-dark-black dark:text-white-one">{title}</h1>
-            <p className=" text-dark-black dark:text-white-one">{subtitle}</p>
+          </div>
+
+          {/* Link Buttons */}
+          <div className="flex justify-center items-center gap-4">
+            <a
+              href={demolink}
+              className="btn rounded-lg px-4 py-2"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>Demo</p>
+              <BiLinkExternal />
+            </a>
+            <a
+              href={githublink}
+              className="btn rounded-lg px-4 py-2"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>Code</p>
+              <FaGithub />
+            </a>
           </div>
 
           {/* Tech Stack */}
@@ -74,28 +96,6 @@ const Project = ({ project }) => {
                 </div>
               );
             })}
-          </div>
-
-          {/* Link Buttons */}
-          <div className="flex justify-center items-center gap-4">
-            <a
-              href={demolink}
-              className="btn rounded-lg px-4 py-2"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <p>Demo</p>
-              <BiLinkExternal />
-            </a>
-            <a
-              href={demolink}
-              className="btn rounded-lg px-4 py-2"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <p>Code</p>
-              <FaGithub />
-            </a>
           </div>
 
           {/* Content */}
