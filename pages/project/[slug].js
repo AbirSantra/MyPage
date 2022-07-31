7072f80fdd7a8b512d43a595d3aec255342ce7b5
Abir Santra <santraabir8@gmail.com>
2022-07-31 19:46:38 +0530

    Removed comments

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -20,25 +20,6 @@ const Project = ({ project }) => {
 
   return (
     <div className="w-full flex justify-center items-center py-12">
-      {/* <h1>{project.title}</h1>
-      <h2>{project.subtitle}</h2>
-      <img src={urlFor(project.coverImage)} alt="project" />
-      <a href={project.githublink}>Github Link</a>
-      <a href={project.demolink}>Demo Link</a>
-      <PortableText
-        projectId="kmgaqrem"
-        dataset="production"
-        // Pass in block content straight from Sanity.io
-        content={project.content}
-        // Optionally override marks, decorators, blocks, etc. in a flat
-        // structure without doing any gymnastics
-        serializers={{
-          h1: (props) => <h1 style={{ color: "red" }} {...props} />,
-          li: ({ children }) => (
-            <li className="special-list-item">{children}</li>
-          ),
-        }}
-      /> */}
       <div className="w-full max-w-[1440px] flex flex-col justify-center items-center gap-16 px-4 md:px-8 lg:px-16">
         <Head>
           <title>Abir Santra | Project</title>
